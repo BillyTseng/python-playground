4ea439b06ceb8d29aea0e5ebc06e6e136281d48a
Billy Tseng <tseng.billy@gmail.com>
2017-11-06 08:34:14 -0700

    feat: implement question 2.

--- solutions.py
+++ solutions.py
@@ -1,6 +1,4 @@
 #! /usr/bin/env python
-
-
 """
 Question 1
 Given two strings s and t, determine whether some anagram of t is a substring
@@ -48,13 +46,14 @@ def hashmap_helper(hashmap, string):
     for char in string:
         hashmap[ord(char) % 52] += 1
 
+
 # Test cases of question 1:
-# Case 1: empty source or empty target
+# Case 1: edge case, empty source or empty target
 s = ""
 t = ""
 print "Question 1 case 1 should be False: {}".format(question1(s, t))
 
-# Case 2: source is shorter than target.
+# Case 2: edge case, source is shorter than target.
 s = "aa"
 t = "bbb"
 print "Question 1 case 2 should be False: {}".format(question1(s, t))
@@ -68,3 +67,45 @@ print "Question 1 case 3 should be True: {}".format(question1(s, t))
 s = "Google"
 t = "OOgl"
 print "Question 1 case 4 should be False: {}".format(question1(s, t))
+
+"""
+Question 2
+Given a string a, find the longest palindromic substring contained in a.
+Your function definition should look like question2(a), and return a string.
+"""
+
+
+def question2(string):
+    return palindrome_helper(string)
+
+
+def palindrome_helper(string):
+    # the edge cases: empty string and one char string.
+    if len(string) <= 1:
+        return string
+    elif string[0] == string[-1]:
+        middle_str = palindrome_helper(string[1:-1])
+        return string[0] + middle_str + string[-1]
+    else:
+        begin_str = palindrome_helper(string[1:])
+        end_str = palindrome_helper(string[:-1])
+        if len(begin_str) >= len(end_str):
+            return begin_str
+        else:
+            return end_str
+
+
+# Test cases of question 1:
+# Case 1: edge case, empty string.
+print "Question 2 case 1 should be empty string: {}".format(question2(""))
+
+# Case 2: edge case, one char string.
+print "Question 2 case 2 should be A: {}".format(question2("A"))
+
+# Case 3: race13car11
+print "Question 2 case 3 should be rac3car: {}".format(
+                                                    question2("race13car11"))
+
+# Case 4: 8884321123777
+print "Question 2 case 4 should be 321123: {}".format(
+                                                    question2("8884321123777"))
