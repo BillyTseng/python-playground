7330ea67809b0bc938cd9eeeebe34e747b1a7972
Billy Tseng <tseng.billy@gmail.com>
2017-11-07 02:10:59 -0700

    feat: implement question 4.

--- solutions.py
+++ solutions.py
@@ -95,7 +95,7 @@ def palindrome_helper(string):
             return end_str
 
 
-# Test cases of question 1:
+# Test cases of question 2:
 # Case 1: edge case, empty string.
 print "Question 2 case 1 should be empty string: {}".format(question2(""))
 
@@ -109,3 +109,73 @@ print "Question 2 case 3 should be rac3car: {}".format(
 # Case 4: 8884321123777
 print "Question 2 case 4 should be 321123: {}".format(
                                                     question2("8884321123777"))
+
+"""
+Question 3
+Given an undirected graph G, find the minimum spanning tree within G.
+"""
+
+
+def question3(G):
+    sorted_list = sort_nodes_by_weight(G)
+    # Because of undriected graph, need to divide 2
+    # the least number of edge will be number of vertex - 1
+    if len(sorted_list)/2 > (len(G) - 1):
+        sorted_list = sorted_list[:(len(G) - 1)*2]
+
+    sorted_list = transfer_to_graph(sorted_list)
+    return sorted_list
+
+
+def sort_nodes_by_weight(G):
+    """
+    sort_nodes_by_weight sort nodes of graph, G, by ascending order in weight.
+    arg: G as inputed graph.
+    return: a list of nodes which is sorted by edge's weight.
+    """
+    nodes = []
+    for k, v in G.items():
+        if len(v) == 1:
+            nodes.append([k, v[0][0], v[0][1]])
+        else:
+            for i in v:
+                nodes.append([k, i[0], i[1]])
+
+    nodes = sorted(nodes, key=lambda item: item[2])
+    return nodes
+
+
+def transfer_to_graph(input_list):
+    """
+    transfer_to_graph exchange list to graph.
+    arg: inputed list
+    return: graph
+    """
+    graph = {}
+    for item in input_list:
+        if item[0] not in graph.keys():
+            graph[item[0]] = []
+        graph[item[0]].append((item[1], item[2]))
+
+    return graph
+
+
+# Test cases of question 3:
+# Case 1: edge case, empty graph.
+print "Question 3 case 1 should be empty graph: {}".format(question3({}))
+
+# Case 2: edge case, it is already a minimum spanning tree.
+G = {'A': [('B', 2)], 'B': [('A', 2), ('C', 5)], 'C': [('B', 5)]}
+print "Question 3 case 2 should be "
+print "{'A': [('B', 2)],"
+print " 'C': [('B', 5)],"
+print " 'B': [('A', 2), ('C', 5)]}}: {}".format(question3(G))
+
+# Case 3
+G = {'A': [
+    ('B', 2), ('C', 1)], 'B': [('A', 2), ('C', 5)], 'C': [('B', 5), ('A', 1)]}
+
+print "Question 3 case 2 should be "
+print "{'A': [('C', 1), ('B', 2)],"
+print " 'C': [('A', 1)],"
+print " 'B': [('A', 2)]}}: {}".format(question3(G))
